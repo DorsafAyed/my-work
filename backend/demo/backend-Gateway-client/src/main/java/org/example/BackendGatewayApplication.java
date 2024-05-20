c9bbdffbff08d86becb8872caf962fb2251baefc
DorsafAyed <dorsafayed816@gmail.com>
2024-05-20 02:06:03 +0200

    backend

---
+++ backend/demo/backend-Gateway-client/src/main/java/org/example/BackendGatewayApplication.java
@@ -0,0 +1,15 @@
+package org.example;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class BackendGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendGatewayApplication.class, args);
+    }
+
+}
